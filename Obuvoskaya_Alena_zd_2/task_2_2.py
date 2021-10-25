80e9ae3b60cd113a66ca4d2e85ddf8e896fbdb0f
Alenanka <89450782+Alenanka@users.noreply.github.com>
2021-10-25 14:16:28 +0300

    Update task_2_2.py

--- Obuvoskaya_Alena_zd_2/task_2_2.py
+++ Obuvoskaya_Alena_zd_2/task_2_2.py
@@ -1,16 +1,16 @@
-spisok = ['в', '5', 'часов', '17', 'минут', 'температура', 'воздуха', 'была', '+5', 'градусов']
+list_weather = ['в', '5', 'часов', '17', 'минут', 'температура', 'воздуха', 'была', '+5', 'градусов']
 i = 0
-while i < len(spisok):
-    if ((spisok[i][0] in '+-') and spisok[i][1:].isdigit()) or (spisok[i]).isdigit():
-        if spisok[i][0] in '+-' and len(spisok[i][1:]) < 2:
-            spisok[i] = spisok[i][0] + "0" + spisok[i][1:]
-        if len(spisok[i]) < 2:
-            spisok[i] = "0" + spisok[i]
-        spisok.insert(i, '"')
-        spisok.insert(i + 2, '"')
+while i < len(list_weather):
+    if ((list_weather[i][0] in '+-') and list_weather[i][1:].isdigit()) or (list_weather[i]).isdigit():
+        if list_weather[i][0] in '+-' and len(list_weather[i][1:]) < 2:
+            list_weather[i] = list_weather[i][0] + "0" + list_weather[i][1:]
+        if len(list_weather[i]) < 2:
+            list_weather[i] = "0" + list_weather[i]
+        list_weather.insert(i, '"')
+        list_weather.insert(i + 2, '"')
         i = i + 2
     i = i+1
-print(spisok)
+print(list_weather)
 
-stt = ' '.join(spisok)
-print(stt)
+str_weather = ' '.join(list_weather)
+print(str_weather)
